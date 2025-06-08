a0fe8038a7bb275c29e5e03cf4739999280b6c3b
TheZenAku <rafaelrodrigues-2022@hotmail.com>
2025-06-08 16:21:42 -0300

    #Adicionado nova aplicação
    Add MyConsoleAPP project with basic console output
    
    - Introduced a new project "MyConsoleAPP" in `Primeiro.sln`.
    - Updated solution configuration for Debug and Release builds.
    - Created `MyConsoleAPP.csproj` targeting .NET 9.0 with necessary settings.
    - Added `Program.cs` with a simple console application printing four lines.

---
+++ MyConsoleAPP/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MyConsoleAPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeiro");
+            Console.WriteLine("Segundo");
+            Console.WriteLine("Terceiro");
+            Console.WriteLine("Quarto");
+        }
+    }
+}
