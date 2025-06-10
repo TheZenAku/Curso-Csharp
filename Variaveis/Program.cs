47d9c8c8bd55ce36b6e972fe5dde66ac67c0f47b
TheZenAku <rafaelrodrigues-2022@hotmail.com>
2025-06-10 11:23:58 -0300

    Add integral data type declarations in Program.cs
    This commit introduces several signed and unsigned integral
    data type declarations in the `Main` method of the `Program`
    class. It includes variables of types `sbyte`, `short`, `int`,
    `long`, `byte`, `ushort`, `uint`, and `ulong`, with comments
    indicating their value ranges. Additionally, it demonstrates
    operations on a `sbyte` variable named `numero`, showcasing
    assignment and console output, along with clarifying comments.

--- Variaveis/Program.cs
+++ Variaveis/Program.cs
@@ -10,6 +10,24 @@ namespace Variaveis
     {
         static void Main(string[] args)
         {
+            // integral assinado
+            sbyte a = 10; // -128 a 127
+            short b = 20; // -32.768 a 32.767
+            int c = 30; // -2.147.483.648 a 2.147.483.647
+            long d = 40; // -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+
+            // integral nao assinado
+            byte e = 50; // 0 a 255
+            ushort f = 60; // 0 a 65.535
+            uint g = 70; // 0 a 4.294.967.295
+            ulong h = 80; // 0 a 18.446.744.073.709.551.615
+
+            sbyte numero = 100; // -128 a 127
+            numero = 50; // o valor de numero agora é 50
+            numero = a; // Atribuindo o valor de 'a' a 'numero'
+
+            Console.WriteLine(numero);
+            Console.ReadKey(); // Mantem o console aberto para ver o resultado
 
         }
     }
