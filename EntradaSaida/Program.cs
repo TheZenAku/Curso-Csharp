603086c846bfff7d01fb9d49c3bf5bf7c8c78b4a
TheZenAku <rafaelrodrigues-2022@hotmail.com>
2025-06-21 01:33:03 -0300

    Refactor console output in Program.cs
    Comment out previous console display and input lines for future reference.

--- EntradaSaida/Program.cs
+++ EntradaSaida/Program.cs
@@ -10,7 +10,18 @@ namespace EntradaSaida
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            //Console.WriteLine("Gabriel"); //Exibe o texto "Gabriel" no console
+            //Console.Write("Gabriel"); //Exibe o texto "Gabriel" no console sem pular linha
+            //Console.ReadKey(); // Mantém o console aberto até que uma tecla seja pressionada
+
+            //Console.Read(); // Mantém o console aberto até que uma tecla seja pressionada
+
+            //int codigo = Console.Read(); // Lê o código ASCII da tecla pressionada
+            //Console.Write(codigo); // Exibe o código ASCII da tecla pressionada
+
+            //string texto = Console.ReadLine(); // Mantém o console aberto até que uma tecla seja pressionada
+            //Console.Write(texto);
+
         }
     }
 }
