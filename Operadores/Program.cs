e6ac27076b1fd6b6404635325c677a1cadeaaec6
TheZenAku <rafaelrodrigues-2022@hotmail.com>
2025-06-24 12:02:09 -0300

    Add arithmetic operations for num1 and num2
    Introduced new variables `num1` and `num2` with values 23 and 67.
    Updated arithmetic operations to calculate and print the results
    for addition, subtraction, multiplication, division, and modulo.
    Removed the previous operation (10 + 2) from the code.

--- Operadores/Program.cs
+++ Operadores/Program.cs
@@ -11,9 +11,30 @@ namespace Operadores
         static void Main(string[] args)
         {
             #region operadores aritméticos
-            int resultado = 10 + 2;
-            Console.WriteLine(resultado);
-            Console.ReadKey();
+            //int num1 = 23;
+            //int num2 = 67;
+
+            //int resultado = num1 + num2;
+            //Console.Write("soma: ");
+            //Console.WriteLine(resultado);
+
+            //resultado = num1 - num2;
+            //Console.Write("subtração: ");
+            //Console.WriteLine(resultado);
+
+            //resultado = num1 * num2;
+            //Console.Write("multiplicação: ");
+            //Console.WriteLine(resultado);
+
+            //resultado = num1 / num2;
+            //Console.Write("divisão: ");
+            //Console.WriteLine(resultado);
+
+            //resultado = num1 % num2; //retorna o resto da divisão apenas
+            //Console.Write("módulo: ");
+            //Console.WriteLine(resultado);
+
+            //Console.ReadKey();
             #endregion
         }
     }
