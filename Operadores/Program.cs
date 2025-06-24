f9a436ed62d1a19ecedc59b2d77ffa97625b4852
TheZenAku <rafaelrodrigues-2022@hotmail.com>
2025-06-24 11:55:52 -0300

    Add new "Operadores" project with basic implementation
    This commit introduces a new project named "Operadores" to the solution. It includes the project file (`Operadores.csproj`), configuration settings, a basic implementation in `Program.cs`, an `App.config` for configuration, and assembly information in `AssemblyInfo.cs`.

---
+++ Operadores/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region operadores aritméticos
+            int resultado = 10 + 2;
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
