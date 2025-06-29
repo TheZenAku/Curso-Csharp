1b50d4c796909c89b98a1e801a71cf80959e64f1
TheZenAku <rafaelrodrigues-2022@hotmail.com>
2025-06-29 18:09:26 -0300

    Add increment operator examples in Program.cs
    Introduce a new region titled "operadores de incremento e decremento" in `Program.cs`. This section contains commented-out code demonstrating the use of pre-increment and post-increment operators in C#. The examples include console output statements to illustrate the results, serving as instructional material for future reference.

--- Operadores/Program.cs
+++ Operadores/Program.cs
@@ -36,6 +36,21 @@ namespace Operadores
 
             //Console.ReadKey();
             #endregion
+
+            #region operadores de incremento e decremento
+
+            //int num1 = 10;
+            ////num1 = num1 + 1;
+            ////++num1; //incrementa 1 unidade ao valor da variável num1
+
+            //Console.WriteLine(++num1); //incrementa 1 unidade ao valor da variável num1 e exibe o resultado
+            //Console.WriteLine(num1++); //exibe o valor da variável num1 e DEPOIS incrementa 1 unidade ao seu valor
+            //Console.Write("agora sim acrescenta +1: ");
+            //Console.WriteLine(num1);
+            //Console.ReadKey();
+            #endregion
+
+
         }
     }
 }
