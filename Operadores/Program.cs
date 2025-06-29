3032151966f70b5d2d51dd230bae9e9efe07e7c9
TheZenAku <rafaelrodrigues-2022@hotmail.com>
2025-06-29 18:59:08 -0300

    Add operator demonstration regions to Program.cs
    Enhanced Program.cs with new regions showcasing C# operators, including concatenation, assignment, equality, relational, and logical operators. Each region contains commented-out examples for better understanding and readability.

--- Operadores/Program.cs
+++ Operadores/Program.cs
@@ -50,6 +50,77 @@ namespace Operadores
             //Console.ReadKey();
             #endregion
 
+            #region operadores de concatenação
+            //string nome = "Gabriel";
+            //string sobrenome = "artigas";
+            //string nomecompleto = nome + " " + sobrenome;
+            //Console.WriteLine(nomecompleto);
+
+            //Console.WriteLine(100 + 50);
+            //Console.ReadKey();
+            #endregion
+
+            #region operadores de atriuição
+            ////int num1 = 30
+            ////num1 += 20;
+            ////num1 -= 2;
+            ////num1 *= 3;
+            ////num1 /= 2;
+            ////num1 %= 3;
+
+            //string nome = "Gabriel";
+            //nome += " Artigas";
+
+
+            //Console.WriteLine(nome);
+            //Console.ReadKey();
+            #endregion
+
+            #region operadores de igualdade
+
+            //////bool res = 100 == 50; // verifica se 100 é igual a 50
+
+            ////int num1 = 2;
+            ////int num2 = 2;
+            ////res = num1 == num2; // verifica se num1 é igual a num2
+
+            //bool res = 200 != 100 + 99;
+
+            //string nome = "Gabriel";
+            //bool res2 = nome == "Lucas";
+
+
+            //Console.WriteLine("Valor da variavel res " + res);
+            //Console.WriteLine("Valor da variavel res2 " + res2);
+            //Console.ReadKey();
+            #endregion
+
+            #region operadores relacionais
+            //bool res1 = 100 < 50 + 65 * 2;
+            //bool res2 = 100 > 50 * 2;
+            //bool res3 = 100 + 25 >= 50 * 2;
+            //Console.WriteLine("Valor da variável res3 " + res1);
+            //Console.WriteLine("Valor da variável res4 " + res2);
+            //Console.WriteLine("Valor da variável res5 " + res3);
+            //Console.ReadKey();
+            #endregion
+
+            #region operadores lógicos
+
+            //bool res1 = 100 > 50;
+            //bool res2 = 100 == 100;
+
+            //bool final1 = res1 && res2; // operador lógico E (AND) - ambos devem ser verdadeiros para retornar verdadeiro
+            //bool final2 = res1 != res2; // operador lógico diferente de
+            //bool final3 = res1 || res2; // operador lógico OU
+
+            //Console.WriteLine("Resultado final1 " + final1);
+            //Console.WriteLine("Resultado final2 " + final2);
+            //Console.WriteLine("Resultado final3 " + final3);
+            //Console.ReadKey();
+
+            #endregion
+
 
         }
     }
