69acefe92452dde36a73681bdbeb07e46876a474
TheZenAku <rafaelrodrigues-2022@hotmail.com>
2025-07-01 12:23:54 -0300

    Add new "Colecao" project with initial setup
    - Created project file `Colecao.csproj` and added to solution.
    - Updated solution configuration for new project build settings.
    - Added `App.config` to specify supported runtime version.
    - Implemented `Program.cs` with a template for a console app.
    - Included `AssemblyInfo.cs` for assembly metadata.

---
+++ Colecao/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Array de Inteiros
+            //int[] numeros = new int[5]; // Array de inteiros com 5 posições
+            //numeros[0] = 10; // Atribuindo valor à primeira posição do array
+            //numeros[1] = 20; // Atribuindo valor à segunda posição do array
+            //numeros[2] = 30; // Atribuindo valor à terceira posição do array
+            //numeros[3] = 40; // Atribuindo valor à quarta posição do array
+            //numeros[4] = 50; // Atribuindo valor à quinta posição do array
+
+            //numeros[2] = 55; // Modificando o valor da terceira posição do array
+
+            //Console.WriteLine(numeros[2]);
+
+            //string[] nomes = new string[3]; // Array de strings com 3 posições
+            //nomes[0] = "João"; // Atribuindo valor à primeira posição do array
+            //nomes[1] = "Maria"; // Atribuindo valor à segunda posição do array
+            //nomes[2] = "José"; // Atribuindo valor à terceira posição do array
+
+            //Console.WriteLine(nomes[1]); // Exibindo o valor da segunda posição do array
+            //Console.ReadKey();
+
+            ////atribuindo valores ao array já na criação
+            //string[] frutas = new string[3] 
+            //{
+            //    "Maçã",
+            //    "Banana",
+            //    "Laranja"
+            //};
+            //frutas[0] = "Pera";
+
+            //Console.WriteLine(frutas[0]); // Exibindo o valor da primeira posição do array
+            //Console.WriteLine(frutas[1]); // Exibindo o valor da segunda posição do array
+            //Console.WriteLine(frutas[2]); // Exibindo o valor da terceira posição do array
+            #endregion
+
+
+        }
+    }
+}
