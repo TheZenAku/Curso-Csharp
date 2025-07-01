a188cb800ab485cba681f402e17b2567aad01d5a
TheZenAku <rafaelrodrigues-2022@hotmail.com>
2025-07-01 12:00:20 -0300

    Add ConversorTemperaturas project for temperature conversion
    This commit introduces a new project named "ConversorTemperaturas" to the solution. The project targets .NET Framework version 4.8 and includes essential library references. It features a `Program.cs` file that implements a temperature conversion application, converting Celsius to Fahrenheit and Kelvin. An `App.config` file is added for configuration settings, and the `AssemblyInfo.cs` file is updated with relevant assembly metadata.

---
+++ ConversorTemperaturas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConversorTemperaturas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //double c, f, k;
+            //Console.WriteLine("CONVERSOR DE TEMPERATURAS");
+            //Console.Write("Insira a temperatura em Celsius: ");
+            //c = double.Parse(Console.ReadLine());
+            //Console.WriteLine("-------------------------------");
+
+            //// (c * 9 / 5) + 32 = f
+            //f = (c * 9 / 5) + 32;
+
+            //// c + 273.15 = k
+            //k = c + 273.15;
+
+            //Console.WriteLine("Temperatura em Fahrenheit: " + f);
+            //Console.WriteLine("Temperatura em Kelvin: " + k);
+            //Console.WriteLine("-------------------------------");
+            //Console.ReadKey();
+
+        }
+    }
+}
